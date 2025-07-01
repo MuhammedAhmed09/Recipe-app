5ec3eb003190620a85526add15d175f6dc98217f
[Muhammed] <[nameismilo2@gmail.com]>
2025-07-01 20:08:55 +0300

    update responive nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { FaBars } from "react-icons/fa6";
+import { IoClose } from "react-icons/io5";
+
+// import whiteLogo from '/image/whitecustomlogo.svg'
+import blackLogo from '/image/blackcutomlogo.svg'
 
 const navItems = {
     links: [
@@ -10,33 +15,54 @@ const navItems = {
 }
 
 const Navbar = () => {
-    const [openNav, setOpenNav] = React.useState(false);
-    const [selectedLink, setSelectedLink] = React.useState(0);
+    const [selectedLink, setSelectedLink] = React.useState(0);    
+    const [isOpen, setIsOpen] = React.useState(false);
 
-    React.useEffect(() => {
-        window.addEventListener(
-            "resize",
-            () => window.innerWidth >= 960 && setOpenNav(false),
-        );
-    }, []);
+    const handleLinkClick = (index) => {
+        setSelectedLink(index);
+        setIsOpen(false)
+    }
 
-
-    
+    const handleBarsClick = () => {
+        setIsOpen(prev => !prev)
+    }
 
   return (
-    <nav className='bg-emerald-300 text-white shadow-2xl items-center py-2 px-[15%]'>
-        {!openNav ? (
-            <ul className='w-full flex 2xl:text-2xl justify-center'>
-            {navItems.links.map((item, index) => (
-                <li key={index} className={selectedLink == index ? 'px-4 py-1 mx-4 bg-emerald-400 rounded-2xl' : 'px-4 py-1 mx-4 hover:bg-emerald-400 rounded-2xl'}  onClick={() => setSelectedLink(index)} >
-                    <Link to={item.path}>{item.component}</Link>
-                </li>
-            ))}
-        </ul>
-        ): (
-            <h3>bars</h3>
+    <nav className="bg-emerald-300 text-white shadow-2xl py-2 px-4 sm:px-8 md:px-[10%] lg:px-[15%]">
+        <div className="flex items-center justify-between w-full">
+            <Link to='/'>
+                <img src={blackLogo} width={150} alt="logo"/>
+            </Link>
+
+            <div className='2xl:text-2xl flex items-center'>
+                <button className='lg:hidden mr-4' onClick={handleBarsClick}>
+                    <p className='font-bold text-2xl'>
+                        {isOpen ? <IoClose/> : <FaBars/>}
+                    </p>
+                </button>
+
+                <ul className={`${isOpen ? ' flex flex-col absolute top-20 left-0 w-full h-[50vh] justify-evenly bg-emerald-400 p-4 rounded-b-xl z-10 ' : 'hidden'} lg:flex lg:flex-row lg:static lg:bg-transparent`}>
+                    {navItems.links.map((item, index) => (
+                        <li 
+                            key={index} 
+                            className={`${selectedLink === index ? 'bg-white text-emerald-700 font-bold' : ' hover:bg-emerald-400'} px-4 py-1 mx-4 rounded-2xl`}  >
+                            <Link to={item.path} onClick={() => handleLinkClick(index)}>{item.component}</Link>
+                        </li>
+                    ))}
+                </ul>
+                {/* Auth */}
+                <div className="hidden lg:block ml-4">
+                    <h2>Auth</h2>
+                </div>
+            </div>
+        </div>
+
+        {/* Auth in mobile */}
+        {isOpen && (
+            <div className="lg:hidden mt-2 text-center">
+            <h2>Auth</h2>
+            </div>
         )}
-        
     </nav>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+
+import B7R from '/image/B7R0.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex justify-center align-middle p-4 bg-emerald-950 h-[100vh]'>
+      <a target="_blank" href="https://www.instagram.com/3mr___7e/">
+        <img src={B7R} alt="B7R" width={350} />
+        <p className='font-bold text-3xl'>عشان انته تخين وعجل عملتلك دا</p>
+      </a>
+    </div>
   )
 }
 
